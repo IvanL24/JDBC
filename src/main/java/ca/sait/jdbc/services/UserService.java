58b0e1d80c04f526f661c4149fb1eef858705461
IvanL24 <lavenaivan@gmail.com>
2022-03-11 13:08:16 -0700

    Created RoleServices.java inside the package services.

--- src/main/java/ca/sait/jdbc/services/UserService.java
+++ src/main/java/ca/sait/jdbc/services/UserService.java
@@ -6,31 +6,31 @@ import ca.sait.jdbc.models.User;
 import java.util.List;
 
 public class UserService {
-    private UserDB UserDB = new UserDB();
+    private UserDB userDB = new UserDB();
     
     public User get(String email) throws Exception {
-        User user = this.UserDB.get(email);
+        User user = this.userDB.get(email);
         return user;
     }
     
     public List<User> getAll() throws Exception {
-        List<User> users = this.UserDB.getAll();
+        List<User> users = this.userDB.getAll();
         return users;
     }
     
     public boolean insert(String email, boolean active, String firstName, String lastName, String password, Role role) throws Exception {
         User user = new User(email, active, firstName, lastName, password, role);
-        return this.UserDB.insert(user);
+        return this.userDB.insert(user);
     }
     
     public boolean update(String email, boolean active, String firstName, String lastName, String password, Role role) throws Exception {
         User user = new User(email, active, firstName, lastName, password, role);
-        return this.UserDB.update(user);
+        return this.userDB.update(user);
     }
     
     public boolean delete(String email) throws Exception {
         User user = new User();
         user.setEmail(email);
-        return this.UserDB.delete(user);
+        return this.userDB.delete(user);
     }
 }
